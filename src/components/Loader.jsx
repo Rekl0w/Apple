b48e6a9a64aff17fe317fcf834c94127c386f3d4
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-03-21 14:37:37 +0300

    Add Model component and update dependencies

---
+++ src/components/Loader.jsx
@@ -0,0 +1,13 @@
+const Loader = () => {
+  return (
+    <HTML>
+      <div className="absolute top-0 left-0 w-full h-full flex justify-center items-center">
+        <div className="w-[10vw] h-[10vw] rounded-full ">
+            Loading...
+        </div>
+      </div>
+    </HTML>
+  );
+};
+
+export default Loader;
