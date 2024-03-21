60c0964ca6e0ec97cd7920bb8c6a06e05454d813
Batuhan Tomo <batuhan.tomo@gmail.com>
2024-03-21 14:40:31 +0300

    Fix import and formatting issues in Loader and Model components

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,13 +1,15 @@
 import {
-  Loader,
+  Html,
   OrbitControls,
   PerspectiveCamera,
   View,
 } from "@react-three/drei";
-import Lights from "./Lights";
-import { Suspense } from "react";
-import IPhone from "./IPhone";
+
 import * as THREE from "three";
+import Lights from "./Lights";
+import Loader from "./Loader";
+import IPhone from "./IPhone";
+import { Suspense } from "react";
 
 const ModelView = ({
   index,
@@ -24,7 +26,9 @@ const ModelView = ({
       id={gsapType}
       className={`w-full h-full absolute ${index === 2 ? "right-[-100%]" : ""}`}
     >
+      {/* Ambient Light */}
       <ambientLight intensity={0.3} />
+
       <PerspectiveCamera makeDefault position={[0, 0, 4]} />
 
       <Lights />
@@ -41,7 +45,7 @@ const ModelView = ({
 
       <group
         ref={groupRef}
-        name={`${index === 1} ? 'small' : 'large'`}
+        name={`${index === 1} ? 'small' : 'large`}
         position={[0, 0, 0]}
       >
         <Suspense fallback={<Loader />}>
